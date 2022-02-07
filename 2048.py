39d9f77678ebaa0f658d0b371e82cccd3cfc930a
takkolucharankumar <tcharankumar16@gmail.com>
2022-02-07 15:22:44 +0530

    Created Random Cell Function
    This method will add new number in random empty cell

--- 2048.py
+++ 2048.py
@@ -6,3 +6,11 @@ class InsideGame:
         self.moved=False
         self.slided=False
         self.merged=False
+    def random_cell(self):
+        cells=[]
+        for i in range(self.n):
+            for j in range(self.n):
+                if(self.gridCells[i][j]==0):
+                    cells.append((i,j))
+        curr=random.choice(cells)
+        self.gridCells[curr[0]][curr[1]]=random.choice([2,2,2,2,2,2,2,2,4])
